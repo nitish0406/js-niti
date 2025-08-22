db5874f206b02f926312ae6faec822320f14e0b8
Nitiraj Menbhurrun <83602488+nitish0406@users.noreply.github.com>
2025-08-22 11:11:23 +0000

    Datatypes Tutorial

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all code of JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3); console.log("Niti") //wrong code readability. code readability should be high
+
+//ECMA Script - standards in JS and need to follow, view website TC39 website
+
+let name="hitesh" //string
+let age=43 //number
+let isLoggedIn=false //boolean
+let state=null
+
+//number=>2 to power 53
+//bigint
+//string => "" use double quote
+//boolean => true/false
+//null => standalone value//empty value
+//undefined=>
+//symbol => unique
+//object e.g null is an object
+
+console.log(typeof undefined);
